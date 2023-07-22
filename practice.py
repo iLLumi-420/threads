43fea13318f6a1b0f41748e3ae6451c2a4ac9d2c
vboxuser <sandeep@Ubuntu.myguest.virtualbox.org>
2023-07-22 22:39:07 +0545

    proper example

--- practice.py
+++ practice.py
@@ -1,26 +1,33 @@
-import threading
+import concurrent.futures
 import time
 
 
 start = time.perf_counter()
 
-def worker():
-    print('Sleeping for 2 second')
-    time.sleep(2)
-    print('Done sleeping')
+def worker(seconds):
+    print(f'Sleeping for {seconds} seconds')
+    time.sleep(seconds)
+    return f'Done sleeping for {seconds}'
+
+seconds = [5,4,3,2,1]
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    results = [executor.submit(worker, sec) for sec in seconds]
+
+    for f in concurrent.futures.as_completed(results):
+        print(f.result())
 
 
-threads = []
-for _ in range(10):
-    t = threading.Thread(target=worker)
-    t.start()
-    threads.append(t)
+# threads = []
+# for _ in range(10):
+#     t = threading.Thread(target=worker)
+#     t.start()
+#     threads.append(t)
 
-for thread in threads:
-    thread.join()
+# for thread in threads:
+#     thread.join()
 
 
 finish = time.perf_counter()
 
 print(f'Completed in {round(finish-start,3)} seconds')
-
