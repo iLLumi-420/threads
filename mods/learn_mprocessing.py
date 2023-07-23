8ebd5df25253df1cd14f28af8bfe5c23559da1d7
iLLumi-420 <sandeepparajuli96@gmail.com>
2023-07-23 14:54:03 +0545

    Pool executor

--- mods/learn_mprocessing.py
+++ mods/learn_mprocessing.py
@@ -1,22 +1,32 @@
-import multiprocessing
+import concurrent.futures
 import time
 
 start = time.perf_counter()
 
 
-def worker():
-    print('Sleeping for 1 seconds')
-    time.sleep(1)
-    print('Done sleeping')
+def worker(seconds):
+    print(f'Sleeping for {seconds} seconds')
+    time.sleep(seconds)
+    return f'Done sleeping for {seconds}'
 
-p1 = multiprocessing.Process(target=worker)
-p2 = multiprocessing.Process(target=worker)
 
-p1.start()
-p2.start()
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    secs = [5, 4, 3, 2, 1]
+    
+    results = executor.map(worker, secs)
+    
+    for result in results:
+        print(result)
 
-p1.join()
-p2.join()
+# processes = []
+# for _ in range(10):
+#     p = multiprocessing.Process(target=worker, args=[1.5])
+#     p.start()
+#     processes.append(p)
+
+# for process in processes:
+#     process.join()
+
 
 
 finish = time.perf_counter()
