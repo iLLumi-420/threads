c8f79365e70a5955ca3c23ea14f1f647fd0df216
iLLumi-420 <sandeepparajuli96@gmail.com>
2023-07-23 11:10:48 +0545

    structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = 'practice',
+    version = '1.0.0',
+    description = 'Learn threads',
+    author='Sandeep Parajuli',
+    packages=['practice'],
+)
