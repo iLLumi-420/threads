6f520b2956a8f0cbd64d875e288dd253d7254fae
iLLumi-420 <sandeepparajuli96@gmail.com>
2023-07-24 13:12:50 +0545

    actual example

--- mods/learn_mprocessing.py
+++ mods/learn_mprocessing.py
@@ -1,31 +1,45 @@
 import concurrent.futures
 import time
+from PIL import Image, ImageFilter
 
 start = time.perf_counter()
 
+size = (1200, 1200)
 
-def worker(seconds):
-    print(f'Sleeping for {seconds} seconds')
-    time.sleep(seconds)
-    return f'Done sleeping for {seconds}'
+img_paths = [
+    'photo-1532009324734-20a7a5813719.jpg',
+    'photo-1516117172878-fd2c41f4a759.jpg',
+    'photo-1524429656589-6633a470097c.jpg',
+    'photo-1530224264768-7ff8c1789d79.jpg',
+    'photo-1564135624576-c5c88640f235.jpg',
+    'photo-1541698444083-023c97d3f4b6.jpg',
+    'photo-1522364723953-452d3431c267.jpg',
+    'photo-1513938709626-033611b8cc03.jpg',
+    'photo-1507143550189-fed454f93097.jpg',
+    'photo-1493976040374-85c8e12f0c0e.jpg',
+    'photo-1504198453319-5ce911bafcde.jpg',
+]
+
+
+def process_image(img_name):
+    try:
+        image = Image.open(img_name)
+        image = image.filter(ImageFilter.GaussianBlur(15))
+        image.thumbnail(size)
+        image.save(f'processed/{img_name}')
+        print('Image was processed')
+    except:
+        print('Processing failed')
 
 
 with concurrent.futures.ProcessPoolExecutor() as executor:
-    secs = [5, 4, 3, 2, 1]
-    
-    results = executor.map(worker, secs)
-    
-    for result in results:
-        print(result)
+    executor.map(process_image, img_paths)
 
-# processes = []
-# for _ in range(10):
-#     p = multiprocessing.Process(target=worker, args=[1.5])
-#     p.start()
-#     processes.append(p)
 
-# for process in processes:
-#     process.join()
+# for url in img_paths:
+#     process_image(url)
+
+
 
 
 
